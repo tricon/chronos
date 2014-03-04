8e414674359d6b825845ba65d48078e6885e93a8
David Aaron Fendley <tricon@me.com>
2014-03-04 13:33:05 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BloodDrive::Application.config.secret_key_base = 'cefeca6d6e2f4cde70345a5bf81c93c535788b761c5f743ec709df53b81a46910e8d085bbd976f8ac0a5487370bc641fb18c273374bd7a68750e21f190b84eb8'
