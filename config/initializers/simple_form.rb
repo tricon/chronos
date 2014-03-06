71e5d3eb83d09cdd28162c05d2e54953ed9d7df8
David Aaron Fendley <tricon@me.com>
2014-03-05 16:24:04 -0800

    Resolve conflict with Formtastic.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,33 +1,9 @@
 # http://stackoverflow.com/questions/14972253/simpleform-default-input-class
 # https://github.com/plataformatec/simple_form/issues/316
-
-inputs = %w[
-  CollectionSelectInput
-  DateTimeInput
-  FileInput
-  GroupedCollectionSelectInput
-  NumericInput
-  PasswordInput
-  RangeInput
-  StringInput
-  TextInput
-]
-
-inputs.each do |input_type|
-  superclass = "SimpleForm::Inputs::#{input_type}".constantize
-
-  new_class = Class.new(superclass) do
-    def input_html_classes
-      super.push('form-control')
-    end
-  end
-
-  Object.const_set(input_type, new_class)
-end
-
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   config.boolean_style = :nested
+  config.input_class = 'form-control'
 
   config.wrappers :bootstrap3, tag: 'div', class: 'form-group', error_class: 'has-error',
       defaults: { input_html: { class: 'default_class' } } do |b|
