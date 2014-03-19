8241344f5cf222f00bc9fce0495e00e5b43a6117
David Aaron Fendley <tricon@me.com>
2014-03-18 19:47:46 -0700

    Spacing.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@blooddrive.me"
 
-  def appointment_confirmation(user,drive_appointment)
+  def appointment_confirmation(user, drive_appointment)
     @user = user
     @drive_appointment = drive_appointment
     mail to: user.email, subject: "#{drive_appointment.drive.name} Blood Drive Appointment Confirmation"
