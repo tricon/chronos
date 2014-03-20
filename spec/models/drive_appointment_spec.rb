7bef961ec909aa8e2173044781d09f8ba43f45bd
David Aaron Fendley <tricon@me.com>
2014-03-20 11:14:27 -0700

    Account for overflow appointments.

--- spec/models/drive_appointment_spec.rb
+++ spec/models/drive_appointment_spec.rb
@@ -7,8 +7,10 @@ describe DriveAppointment do
     end
 
     it "should not save if the maximum number of appointments per slot has been met" do
+      @drive.drive_slots.each { |ds| 2.times { ds.drive_appointments.create!(user_id: 1) }}
+
       lambda {
-        @drive.drive_slots.first.drive_appointments.create
+        @drive.drive_slots.first.reload.drive_appointments.create(user_id: 1)
       }.should_not change(@drive.drive_appointments, :size)
     end
   end
