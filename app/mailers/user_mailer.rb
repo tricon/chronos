80ee7d71f3e8d61fe99d7d01442a380b53d720e4
David Aaron Fendley <tricon@me.com>
2014-03-22 07:30:43 -0700

    Add "Blood Drive Reminder & Eligibility Notice" email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,10 @@ class UserMailer < ActionMailer::Base
     @drive_appointment = drive_appointment
     mail to: user.email, subject: "#{drive_appointment.drive.name} Blood Drive Appointment Confirmation"
   end
+
+  def appointment_reminder_and_eligibility_notice(user, drive_appointment)
+    @user = user
+    @drive_appointment = drive_appointment
+    mail to: user.email, subject: "#{drive_appointment.drive.name} Blood Drive Appointment Reminder & Eligibility Notice"
+  end
 end
